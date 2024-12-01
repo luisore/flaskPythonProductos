5fc7a6ebfb8daca65290feeb3897277e459e2eec
luisore <luissk47@gmail.com>
2024-12-01 14:00:46 -0300

    agrego rutas

---
+++ produtos.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask import jsonify, request
+
+
+productos = [
+    {"id":"1","nombre":"manzana",
+     "cantidad":12},
+     {"id":"2","nombre":"pera",
+     "cantidad":13},
+
+]
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("/index.html")
+
+
+@app.route("/productos", methods = ["GET"])
+def getProductos():
+    return jsonify(productos)
+
+@app.route("/productos", methods=["POST"])
+def postProductos():
+    nuevoProducto = request.json
+    productos.append(nuevoProducto)
+    return "Producto creado correctamente!"
+
+
+@app.route("/productos/<id>", methods=["DELETE"])
+def deleteProductos(id):
+    for prod in productos:
+        if prod["id"]==id:
+            productos.remove(prod)
+            return f"Producto con id {id} borrado correctamente"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
